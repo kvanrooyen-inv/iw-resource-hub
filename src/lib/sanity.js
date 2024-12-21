c1f2cea76240d8c32826011c2a713e768b6fdab8
kvanrooyen-inv <keagan.rooyen@invisible.email>
2024-12-21 01:27:29 +0000

    Updated to use env variables

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -1,8 +1,8 @@
 import { createClient } from '@sanity/client';
 
 export const client = createClient({
-  projectId: 'fvzg368t',
-  dataset: 'production',
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: process.env.REACT_APP_SANITY_DATASET,
   useCdn: true, // `false` if you want to ensure fresh data
   apiVersion: '2024-01-01', // use current date in YYYY-MM-DD format
 });
